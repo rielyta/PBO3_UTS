c0cc6a475be178bce2e1b5fc1d8c2cf6e8352d45
rielyta <desi@students.usu.ac.id>
2025-05-01 20:44:06 +0700

    3

--- src/Soal2/ParkirChan/Main.java
+++ src/Soal2/ParkirChan/Main.java
@@ -4,23 +4,23 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-
+    //menyimpan daftar kendaraan
     private final ArrayList<Kendaraan> daftarKendaraan = new ArrayList<>();
-
+    //manual
     public void durasiManual(Kendaraan kendaraan, int jam) {
         kendaraan.setLamaParkir(jam);
         daftarKendaraan.add(kendaraan);
     }
-
+    //dengan waktu/time (jam keluar - jam masuk)
     public void durasiDenganWaktu(Kendaraan kendaraan, int masuk, int keluar) {
         kendaraan.setLamaParkir(keluar - masuk);
         daftarKendaraan.add(kendaraan);
     }
-
+    //Getter mengambil daftar kendaraan
     public ArrayList<Kendaraan> getDaftarKendaraan() {
         return daftarKendaraan;
     }
-
+    //mrnghitung total biaya
     public double hitungTotalBiaya() {
         double total = 0;
         for (Kendaraan k : daftarKendaraan) {
@@ -28,29 +28,30 @@ public class Main {
         }
         return total;
     }
-
+    //Fungsi utama
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         Main app = new Main();
 
         System.out.println("======= Welcome to ParkingChan =======");
-
+        
         while (true) {
+            //input jenis
             System.out.print("\nEnter vehicle type (Motor/Mobil/Truk) : ");
             String jenis = input.nextLine();
-
+            //input metode
             System.out.print("Enter Duration (Manual/Time): ");
             String metode = input.nextLine();
 
             Kendaraan kendaraan;
-
+            //validasi jenis
             try {
                 kendaraan = new Kendaraan(jenis);
             } catch (IllegalArgumentException e) {
                 System.out.println("Jenis kendaraan tidak valid.");
                 continue;
             }
-
+            //menentukan metode input durasi parkir
             try {
                 if (metode.equalsIgnoreCase("Manual")) {
                     System.out.print("Enter Duration (in hour): ");
@@ -79,16 +80,16 @@ public class Main {
                 System.out.println("Input waktu tidak valid.");
                 continue;
             }
-
+            //ringkasan
             System.out.println("\n---- PARKING SUMMARY ----");
             System.out.println("Vehicle Type     : " + kendaraan.getJenis());
             System.out.println("Parking Time     : " + kendaraan.getLamaParkir() + " hour(s)");
             System.out.println("Total Fee        : Rp" + kendaraan.hitungBiaya());
-
+            //menanyakan ingin menambah kendaraan lagi yang akan diproses
             System.out.print("\nAdd another vehicle? (y/n): ");
             if (!input.nextLine().equalsIgnoreCase("y")) break;
         }
-
+        //output akhir
         System.out.println("\n======= FINAL REPORT =======");
         System.out.println("Total Vehicle Final        : " + app.getDaftarKendaraan().size());
         System.out.println("Total Parking Fees Final   : Rp" + app.hitungTotalBiaya());
